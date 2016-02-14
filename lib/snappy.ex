a2706242afdc041ce5c94014634b55c93715ce08
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2016-02-13 16:34:18 -0800

    Snappy should have the supervisor and not the client.

--- lib/snappy.ex
+++ lib/snappy.ex
@@ -12,7 +12,7 @@ defmodule Snappy do
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Snappy.Client.Supervisor]
+    opts = [strategy: :one_for_one, name: Snappy.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
