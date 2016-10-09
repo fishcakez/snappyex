b957867d692083f8f709a159670fc717624edd81
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2016-10-08 20:13:13 -0700

    Test infinity timeout.

--- test/login_test.exs
+++ test/login_test.exs
@@ -20,4 +20,14 @@ defmodule LoginTest do
     end
   end
   #Tests where snappyex cannot connect will fail
+
+ test "infinity timeout", context do
+     opts = [ host: "localhost", clientID: "ElixirClient1|0x" <> Base.encode16(inspect self), 
+     port: 1531, userName: "APP", password: "APP",  security: Snappyex.Model.SecurityMechanism.plain, 
+     tokenSize: 16, useStringForDecimal: false, properties: :dict.new(), timeout: :infinity]
+
+    assert {:ok, pid} = S.start_link(opts)
+    params = Map.put_new(Map.new, :params, Snappyex.Model.Row.new(values: []))
+    assert [[123]] == query("SELECT 123", params)
+  end
 end
